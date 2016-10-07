f9bf6c421a5ee4a53fc173cba0cfad0c597ec594
alienwizard <johanwendin@gmail.com>
2016-10-07 14:01:06 +0200

    changed layout of design 4

--- wp-content/themes/design4_sidebar/header.php
+++ wp-content/themes/design4_sidebar/header.php
@@ -1,9 +1,6 @@
 <!DOCTYPE html>
-
 <html>
-
 <head>
-
 <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width">
     <link rel="profile" href="http://gmpg.org/xfn/11">
@@ -105,8 +102,39 @@
 </head>
 
 <body>
+<div class="page-container">
+    
 
-<header>
+
+<header class="header-mobile">
     
 </header>
+<nav>
+<div class="nav-content" id="sidopanel">
+    <div class="logga">
+        <?php the_custom_logo(); ?>
+    </div>
+        <div class="search">
+            <?php get_search_form(); ?>
+    </div>
+    <div class="menu">
+<?php wp_nav_menu( array( 'theme_location' => 'side_menu', 'menu_class' => 'nav-side', 'menu_class' => 'main_nav', 'container_class' => 'side-menu-container') ); ?>
+    </div>
+    <div class="social">
+    <i class="fa fa-facebook-official" aria-hidden="true"></i>
+    <i class="fa fa-instagram" aria-hidden="true"></i>
+    <i class="fa fa-twitter-square" aria-hidden="true"></i>
+
+    </div>
+    <!--
+    <div class="partners">
+
+<div class="partner"><img src="http://localhost:8080/design/wp-content/uploads/2016/09/partner_logo4.png" alt=""></div>
+<div class="partner"><img src="http://localhost:8080/design/wp-content/uploads/2016/09/intersport.jpg" alt=""></div>
+<div class="partner"><img src="http://localhost:8080/design/wp-content/uploads/2016/09/partner_logo3.png" alt=""></div>
+<div class="partner"><img src="http://localhost:8080/design/wp-content/uploads/2016/09/logo_gotalands.png" alt=""></div>
+<div class="partner"><img src="http://localhost:8080/design/wp-content/uploads/2016/09/engstrom.png" alt=""></div>
+</div>-->
+</div>
+</nav>
 
--- wp-content/themes/design4_sidebar/page.php
+++ wp-content/themes/design4_sidebar/page.php
@@ -1,37 +1,9 @@
 <?php
 get_header();
 ?>
-<article>
-<div class="container">
-<div class="row">
-<div class="col-md-3 col-sm-3" id="sidopanel">
-	<div class="logga">
-		<?php the_custom_logo(); ?>
-	</div>
-		<div class="search">
-			<?php get_search_form(); ?>
-	</div>
-	<div class="menu">
-<?php wp_nav_menu( array( 'theme_location' => 'side_menu', 'menu_class' => 'nav-side', 'menu_class' => 'main_nav', 'container_class' => 'side-menu-container') ); ?>
-	</div>
-	<div class="social">
-	<i class="fa fa-facebook-official" aria-hidden="true"></i>
-	<i class="fa fa-instagram" aria-hidden="true"></i>
-	<i class="fa fa-twitter-square" aria-hidden="true"></i>
-	<i class="fa fa-search" aria-hidden="true"></i>	
-	</div>
-	<!--
-	<div class="partners">
-
-<div class="partner"><img src="http://localhost:8080/design/wp-content/uploads/2016/09/partner_logo4.png" alt=""></div>
-<div class="partner"><img src="http://localhost:8080/design/wp-content/uploads/2016/09/intersport.jpg" alt=""></div>
-<div class="partner"><img src="http://localhost:8080/design/wp-content/uploads/2016/09/partner_logo3.png" alt=""></div>
-<div class="partner"><img src="http://localhost:8080/design/wp-content/uploads/2016/09/logo_gotalands.png" alt=""></div>
-<div class="partner"><img src="http://localhost:8080/design/wp-content/uploads/2016/09/engstrom.png" alt=""></div>
-</div>-->
-</div>
+<article class="site-content" style="background-image:url('http://localhost:8080/design/wp-content/uploads/2016/09/AdobeStock_40515377.jpeg');">
 <div class="col-md-9 col-sm-9" id="content">
-<div class="banner" style="background-image:url('http://localhost:8080/design/wp-content/uploads/2016/09/AdobeStock_40515377.jpeg');">
+<div class="banner" >
 <div class="text">
 	<?php if( have_posts()){
 
@@ -49,19 +21,19 @@ get_header();
 
 	}?>
 </div>
+
+<div class="col-md-4 ">
+	<?php the_field('left-box-3-header');?>
+	<?php the_field('left-box-3-content');?>
+</div>
+<div class="col-md-4 "></div>
+<div class="col-md-4 "></div>
 <div class="blurarea">
 <div class="blurcontainer">
 <div class="blur-bg"></div>
 
 </div>
 </div>
-</div>
-
-
-
-</div>
-
-
 </div>
 </article>
 
