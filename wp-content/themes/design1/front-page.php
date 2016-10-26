5f97b17df3353cc8e49bad009cb26cba23329b22
alienwizard <johanwendin@gmail.com>
2016-10-26 10:38:44 +0200

    css

--- wp-content/themes/design1/front-page.php
+++ wp-content/themes/design1/front-page.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 get_header();
 
@@ -7,8 +7,10 @@ get_header();
 <section class="banner" style="background-image: url('<?php the_post_thumbnail_url( 'full' ); ?>')">
 <div class="overlay"></div>
 <div class="container">
+	
+	<!-- Call to action -->
 	<div class="cta">
-	<a href="tel:030403"><i class="fa fa-phone" aria-hidden="true"></i><h2>Ring oss idag: 08-702 01 81</h2></a>
+
 
 
 </div>
@@ -20,13 +22,13 @@ get_header();
 
 	<div class="container">
 	<div class="left-side-text">
-	
+
 <div class="main-text-wrap welcome">
-		<?php 
-		
+		<?php
+
 			if ( have_posts() ) {
 				while ( have_posts() ) {
-					the_post(); 
+					the_post();
 					//
 					// Post Content here
 					the_content();
